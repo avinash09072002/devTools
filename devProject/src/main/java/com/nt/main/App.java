e55e20030a0a5837b7019d57346ce6e9700c61c9
Avinash Dharmshale <avinashdh2000@gmail.com>
2024-12-13 20:35:08 +0530

    there there by tl

--- devProject/src/main/java/com/nt/main/App.java
+++ devProject/src/main/java/com/nt/main/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("hellow there ");
         System.out.println("chanage in plane");
+        System.out.println("there there");
     }
 }

70e0bb1bdb9ff066cda50e5a4c7cb742034eb3bc
Avinash Dharmshale <avinashdh2000@gmail.com>
2024-12-15 10:36:39 +0530

    by dev1

--- devProject/src/main/java/com/nt/main/mail.java
+++ devProject/src/main/java/com/nt/main/mail.java
@@ -3,7 +3,7 @@ package com.nt.main;
 public class mail {
 	public mail() {
 
-        System.out.println("sending the mail by avinash ");
+        System.out.println("sending the mail by dev1 ");
 
 	}
 

01042b795e70afc0ea9ce384fdf2020f5e32a2bc
Avinash Dharmshale <avinashdh2000@gmail.com>
2024-12-16 13:06:07 +0530

    adf

--- devProject/src/main/java/com/nt/upi/UpiPayment.java
+++ devProject/src/main/java/com/nt/upi/UpiPayment.java
@@ -4,6 +4,7 @@ public class UpiPayment {
 	
 	public String doUpiPayment(String upid,float amount) {
 		
+		System.out.println("rekjdkfj");
 		
 		return "UPI payment is done "+amount+" is done df"; 
 	}
